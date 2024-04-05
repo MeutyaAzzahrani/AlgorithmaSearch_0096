9f07a07fa4e2c61cfe8a48f52e2b10233adf2e6b
LENOVO <MeutyaAzzahrani>
2024-04-05 15:38:49 +0700

    menambahkan program yang ingin dilakukan

--- AlgorithmaSearch_0096/AlgorithmaSearch_0096.cpp
+++ AlgorithmaSearch_0096/AlgorithmaSearch_0096.cpp
@@ -55,6 +55,9 @@ void LinearSearch()
 		if (i == n)
 			cout << "\n" << item << " not found in the array\n";
 			cout << "\nNumber of comparision : " << ctr << endl;
+
+			cout << "\nContinue search (y/n) : ";
+			cin >> ch;
 	}
 }
 
