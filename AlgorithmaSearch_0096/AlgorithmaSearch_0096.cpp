765461fb95bec267b34dc4446a65a1cc13e7b548
LENOVO <MeutyaAzzahrani>
2024-04-05 14:51:44 +0700

    menambahkan perulangan while

--- AlgorithmaSearch_0096/AlgorithmaSearch_0096.cpp
+++ AlgorithmaSearch_0096/AlgorithmaSearch_0096.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
-int i;
+int arr[20]; // Array to be searched
+int n; // Number of element in the array
+int i; // Index of array element
 
-void input() {
-
+void input()
+{
+	while (true)
+	{
+	}
 }
