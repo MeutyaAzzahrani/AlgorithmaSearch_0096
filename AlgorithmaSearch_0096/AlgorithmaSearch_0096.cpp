cb3788e864170335a42b0b7a98d74c1e3d7f9566
LENOVO <MeutyaAzzahrani>
2024-04-05 15:45:47 +0700

    membuat masukan pada void display

--- AlgorithmaSearch_0096/AlgorithmaSearch_0096.cpp
+++ AlgorithmaSearch_0096/AlgorithmaSearch_0096.cpp
@@ -61,7 +61,11 @@ void LinearSearch()
 	} while ((ch == 'y') || (ch == 'Y'));
 }
 
-void dislpay() {
-
+void dislpay() 
+{
+	cout << "Array elements are: ";
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
